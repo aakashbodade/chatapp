3ceb2db34bb19dcb30595d35d6a0502facfe8eca
Navin Bodade <aakashbodade1990@gmail.com>
2021-06-10 11:54:23 +0530

    added firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyA8ryyfUM2Il6rlJYBJE9eeqzsmBC7GBjs",
+  authDomain: "chatapp-b862d.firebaseapp.com",
+  projectId: "chatapp-b862d",
+  storageBucket: "chatapp-b862d.appspot.com",
+  messagingSenderId: "694526778",
+  appId: "1:694526778:web:5b395241353dc038ae6603",
+};
+
+const app = firebase.initializeApp(config);
